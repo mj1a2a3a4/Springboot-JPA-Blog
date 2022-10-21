e2189d460b67334abb1350568042ce1c75524192
MJ <alswnsdl3151@gmail.com>
2022-10-21 20:33:59 +0900

    댓글작성

--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -1,5 +1,7 @@
 package com.cos.blog.service;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,55 +9,73 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cos.blog.model.Board;
+import com.cos.blog.model.Reply;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.BoardRepository;
+import com.cos.blog.repository.ReplyRepository;
 
 //스프링이 컴포넌트 스캔을 통해서 Bean에 등록을 해줌. IoC를 해준다.
 @Service
 public class BoardService {
-	
+
 	@Autowired
 	private BoardRepository boardRepository;
-	
-	//회원가입
+
+	@Autowired
+	private ReplyRepository replyRepository;
+
+	// 회원가입
 	@Transactional
 	public void 글쓰기(Board board, User user) {
 		board.setCount(0);
 		board.setUser(user);
 		boardRepository.save(board);
 	}
-	
-/*	@Transactional(readOnly = true) // Select 할때 트랜잭션 시작, 서비스 종료시에 트랜잭션 종료(정합성)
-	public User 로그인(User user) {
-		return userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword());
-	}*/
-	
+
+	/*
+	 * @Transactional(readOnly = true) // Select 할때 트랜잭션 시작, 서비스 종료시에 트랜잭션 종료(정합성)
+	 * public User 로그인(User user) { return
+	 * userRepository.findByUsernameAndPassword(user.getUsername(),
+	 * user.getPassword()); }
+	 */
+
 	@Transactional(readOnly = true)
-	public Page<Board> 글목록(Pageable pageable){
+	public Page<Board> 글목록(Pageable pageable) {
 		return boardRepository.findAll(pageable);
 	}
-	
+
 	@Transactional(readOnly = true)
 	public Board 글상세보기(int id) {
-		return boardRepository.findById(id)
-				.orElseThrow(()->{
-					return new IllegalArgumentException("글 상세보기 실패 : 아이디를 찾을 수 없습니다.");
-				});
+		return boardRepository.findById(id).orElseThrow(() -> {
+			return new IllegalArgumentException("글 상세보기 실패 : 아이디를 찾을 수 없습니다.");
+		});
 	}
+
 	@Transactional
 	public void 글삭제하기(int id) {
 		boardRepository.deleteById(id);
 	}
-	
+
 	@Transactional
 	public void 글수정하기(int id, Board requestBoard) {
-		Board board = boardRepository.findById(id)
-				.orElseThrow(()->{
-					return new IllegalArgumentException("글 찾기 실패 : 아이디를 찾을 수 없습니다.");
-				}); //영속화 완료
+		Board board = boardRepository.findById(id).orElseThrow(() -> {
+			return new IllegalArgumentException("글 찾기 실패 : 아이디를 찾을 수 없습니다.");
+		}); // 영속화 완료
 		board.setTitle(requestBoard.getTitle());
 		board.setContent(requestBoard.getContent());
-		//해당 함수로 종료시(Service가 종료될때)에 트랜잭션이 Service가 종료될 때
-		//이때 더티체킹이 일어남(자동업데이트가됨) DB flush.
+		// 해당 함수로 종료시(Service가 종료될때)에 트랜잭션이 Service가 종료될 때
+		// 이때 더티체킹이 일어남(자동업데이트가됨) DB flush.
+	}
+
+	@Transactional
+	public void 댓글쓰기(User user, int boardId, Reply requestReply) {
+
+		Board board = boardRepository.findById(boardId).orElseThrow(() -> {
+			return new IllegalArgumentException("댓글 쓰기 실패 : 게시글 id를 찾을 수 없습니다.");
+		});
+
+		requestReply.setUser(user);
+		requestReply.setBoard(board);
+		replyRepository.save(requestReply);
 	}
 }
--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -9,6 +9,9 @@ let index = {
 		$("#btn-update").on("click",()=>{ 
 			this.update();
 		});
+		$("#btn-reply-save").on("click",()=>{ 
+			this.replySave();
+		});
 	},
 	save: function(){
 		let data = {
@@ -68,6 +71,28 @@ let index = {
 		}).done(function(resp){
 			alert("글수정이 완료되었습니다.");
 			location.href="/";
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		}); // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청!
+	},
+	replySave: function(){
+		let data = {
+			content : $("#reply-content").val()
+		};
+		let boardId = $("#boardId").val();
+		//console.log(data);
+		
+		//ajax 호출시 default가 비동기 호출
+		$.ajax({
+			type: "POST",
+			url : `/api/board/${boardId}/reply`,
+			data : JSON.stringify(data), //http body 데이터
+			contentType: "application/json; charset=utf-8", //body데이터가 어떤 타입인지(mime)
+			dataType: "json" //요청을 서버로 해서 응ss답이 왔을 때 기본적으로 모든 것이 String (생긴게 JSON이라면 )=> javascript 오브젝트로 변경
+		}).done(function(resp){
+			alert("댓글 작성이 완료되었습니다.");
+			console.log(resp);
+			location.href=`/board/${boardId}`;
 		}).fail(function(error){
 			alert(JSON.stringify(error));
 		}); // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청!
